b3acc8ca7b1cc93f2a93d13a8d4456188dda9029
brightokike@gmail.com <brightokike@gmail.com>
2020-10-10 16:25:47 +0300

    trying to implement the send notify functionality

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,34 @@
 class Event < ApplicationRecord
   belongs_to :user
+
+  
+  def  self.upcoming_event(evnt)
+    event = evnt.date.split('/')
+    evnt_month = event[0].to_i
+    evnt_day = event[1].to_i
+    evnt_year = event[3].to_i
+
+    today_date = Date.today.strftime()
+    today_date = today_date.split('-')
+    curr_day = today_date[2].to_i
+    curr_month = today_date[1].to_i
+    curr_year = today_date[0].to_i
+    result = false
+    
+    if evnt_day > curr_day && evnt_month == curr_month && evnt_year == curr_year
+      result = true
+    elsif  evnt_month > curr_month && evnt_year >= curr_year
+      result = true
+    elsif evnt_day >= curr_day && evnt_month > curr_month && evnt_year >= curr_year
+      result = true
+    elsif evnt_day > curr_day && evnt_month >= curr_month && evnt_year >= curr_year
+      result = true 
+    elsif evnt_day < curr_day && evnt_month <= curr_month && evnt_year > curr_year
+      result = true
+    elsif evnt_day > curr_day && evnt_month <= curr_month && evnt_year > curr_year
+      result = true
+    end
+    
+    result
+  end
 end
--- telegram/event.rb
+++ telegram/event.rb
@@ -7,8 +7,9 @@ TOKEN = '1173859008:AAH4z5gonpnQSnoL5OQvEcWcp1ROD05cYOs'
 def displayMessage(message)
   Telegram::Bot::Client.run(TOKEN) do |bot|
     bot.api.send_message(chat_id: message.chat.id, text: "Type '/add' to add an event ")
-    bot.api.send_message(chat_id: message.chat.id, text: "Type '/view_events' to view all events")
+    bot.api.send_message(chat_id: message.chat.id, text: "Type '/view_events' to view all events ")
     bot.api.send_message(chat_id: message.chat.id, text: "Type '/delete' to delete event created by you")
+    bot.api.send_message(chat_id: message.chat.id, text: "Type '/notify' to get notify of an upcoming event ")
   end
 end
 
@@ -20,7 +21,8 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
     if !User.exists?(telegram_id: message.from.id)
       user = User.create(telegram_id: message.chat.id, name: message.from.first_name, 
       email: "#{message.chat.username}@gmail.com")
-      bot.api.send_message(chat_id: message.chat.id, text: "Your account has been create with your telegram username")
+      bot.api.send_message(chat_id: message.chat.id, 
+      text: "Welcome @#{message.chat.username} your account has been created and saved in our database")
       displayMessage(message)
     else  
       user = User.find_by(telegram_id: message.chat.id)
@@ -31,7 +33,7 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
       user.events.create(description: message.text)
       user.step = 'date'
       user.save
-      bot.api.send_message(chat_id: message.chat.id, text: "Enter your event date")
+      bot.api.send_message(chat_id: message.chat.id, text: "Enter event date with the following format MM/DD/YYYY")
     when 'date'
       new_event = user.events.last
       new_event.date = message.text
@@ -69,7 +71,7 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
         bot.api.send_message(chat_id: message.chat.id, text: "#{event.date}")
         bot.api.send_message(chat_id: message.chat.id, text: "_________________________")
       end
-        displayMessage(message)
+      displayMessage(message)
       user.step = nil
       user.save
     when '/delete'
@@ -78,6 +80,19 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
       current_user_events = user.events.map{|event| event.description }
       markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: current_user_events)
       bot.api.send_message(chat_id: message.chat.id, text: "select event to delete ", reply_markup: markup)
+    when '/notify'
+      bot.api.send_message(chat_id: message.chat.id, text: "*********************************")
+      bot.api.send_message(chat_id: message.chat.id, 
+      text: "Hello #{message.chat.username} below are the list of upcoming Events. Plan to attend ")
+      bot.api.send_message(chat_id: message.chat.id, text: "**********************************")
+      Event.all.each do |event|
+        p Event.upcoming_event(event)
+        if Event.upcoming_event(event) 
+          bot.api.send_message(chat_id: message.chat.id, text: "#{event.description}")
+          bot.api.send_message(chat_id: message.chat.id, text: "#{event.date}")
+          bot.api.send_message(chat_id: message.chat.id, text: "----------------------------")
+        end
+      end
     end
   end
 end
